832fb86c7c679903a32fe7ac21fe529c68b2a465
DmytryManuylov <95714600+DmytryManuylov@users.noreply.github.com>
2022-01-03 04:31:49 +0300

    Delete README.md

---
+++ Lesson2/Min_number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Min_number
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a, b, c, min = 0;
+            Console.WriteLine("Введите три целых числа: ");
+            Console.WriteLine("Первое: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Второе: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Третье: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (b > a && a < c)
+            {
+                min = a;
+            }
+            if (a > b && b < c)
+            {
+                min = b;
+            }
+            if (a > c && c < b)
+            {
+                min = c;
+            };
+            Console.WriteLine("Наименьшее число {0}", min);
+            Console.ReadLine();
+        }
+    }
+}
